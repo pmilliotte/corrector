bc7955e9431e41f65dddcf6071186d22383b5c74
Pierre Milliotte <pmilliotte@gmail.com>
2024-11-28 14:52:34 +0100

    exam title

--- packages/web/src/pages/Exam.tsx
+++ packages/web/src/pages/Exam.tsx
@@ -2,27 +2,14 @@ import { ReactElement } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { useParams } from 'react-router-dom';
 
-import { ExamFiles } from '~/components/Exams/ExamFiles';
-import { ExamMarks } from '~/components/Exams/ExamMarks';
-import {
-  Separator,
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from '~/components/ui';
-import { trpc, useUserOrganizations } from '~/lib';
+import { trpc, useIntl, useUserOrganizations } from '~/lib';
 
 export const Exam = (): ReactElement => {
   const { examId } = useParams() as { examId: string };
+  const t = useIntl();
   const { selectedOrganization } = useUserOrganizations();
 
   const { data: exam } = trpc.examGet.useQuery({
-    id: examId,
-    organizationId: selectedOrganization.id,
-  });
-
-  const { data: fileNames } = trpc.examFilesGet.useQuery({
     id: examId,
     organizationId: selectedOrganization.id,
   });
@@ -33,8 +20,15 @@ export const Exam = (): ReactElement => {
     <div className="flex flex-col gap-2">
       <div className="flex items-baseline justify-between">
         <div className="font-semibold text-xl">
-          <FormattedMessage id={`common.subjects.${exam.exam.subject}`} /> -{' '}
-          {exam.exam.name}
+          <FormattedMessage
+            id="exams.exam.title"
+            values={{
+              subject: t.formatMessage({
+                id: `common.subjects.${exam.exam.subject}`,
+              }),
+              name: exam.exam.name,
+            }}
+          />
         </div>
         <div className="text-muted-foreground text-sm">
           <FormattedMessage
@@ -45,26 +39,6 @@ export const Exam = (): ReactElement => {
           />
         </div>
       </div>
-      <Separator />
-      <Tabs defaultValue="files" className="space-y-4">
-        <TabsList className="w-full">
-          <TabsTrigger value="files" className="flex-auto">
-            <FormattedMessage id="exams.tabs.files" />
-          </TabsTrigger>
-          <TabsTrigger value="marks" className="flex-auto">
-            <FormattedMessage id="exams.tabs.marks" />
-          </TabsTrigger>
-          <TabsTrigger value="correction" className="flex-auto">
-            <FormattedMessage id="exams.tabs.correction" />
-          </TabsTrigger>
-        </TabsList>
-        <TabsContent value="files" className="space-y-4">
-          <ExamFiles examId={examId} fileNames={fileNames} />
-        </TabsContent>
-        <TabsContent value="marks" className="space-y-4">
-          <ExamMarks examId={examId} fileNames={fileNames} />
-        </TabsContent>
-      </Tabs>
     </div>
   );
 };
