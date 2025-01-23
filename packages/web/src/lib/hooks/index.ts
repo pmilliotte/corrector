3eaa807ceb834d74054b4042a8597d4b3d8b9e30
Pierre Milliotte <pmilliotte@gmail.com>
2025-01-23 15:27:07 +0100

    remove unused files

--- packages/web/src/lib/hooks/index.ts
+++ packages/web/src/lib/hooks/index.ts
@@ -1,6 +1,5 @@
 export * from './useBreakpoint';
 export * from './useMobile';
-export * from './useOnDrop';
 export * from './useOnProblemDrop';
 export * from './useOrganizations';
 export * from './useSidebarItems';
