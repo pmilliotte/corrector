a52283934d97184589dbf1ee866a68dd0f9f88ba
Pierre Milliotte <pmilliotte@gmail.com>
2025-03-05 19:42:29 +0100

    specify how to install chromium

--- packages/functions/src/libs/puppeteer/chromiumPath.ts
+++ packages/functions/src/libs/puppeteer/chromiumPath.ts
@@ -1,2 +1,3 @@
+// npx @puppeteer/browsers install chromium@latest --path /tmp/localChromium
 export const YOUR_LOCAL_CHROMIUM_PATH =
   '/tmp/localChromium/chromium/mac_arm-1424481/chrome-mac/Chromium.app/Contents/MacOS/Chromium';
